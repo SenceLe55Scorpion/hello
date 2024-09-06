7443af8e5f2abe17848035d8d29515a4aa9918a4
khaldoon <kshammo3@huskers.unl.edu>
2024-09-06 03:28:00 +0000

    Thu, Sep 5, 2024, 10:28 PM -05:00

--- hello.c
+++ hello.c
@@ -10,12 +10,44 @@
  */
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 
 int main(int argc, char **argv) {
 
-  printf("Data Science!\n");
+//Daclaring all varables such as location A latitude
 
-printf("Khaldoon Shammo!!!\sn");
+  double d=0;
+  int R=6371;
+  double lat1=0;
+  double lat2=0;
+  double lon1=0;
+  double lon2=0;
+
+  printf ("enter location A's latitude");
+  scanf("%lf", &lat1);
+  printf ("enter location A's longitude");
+  scanf("%lf", &lon1);
+  printf ("enter location B's latitude");
+  scanf("%lf", &lat2);
+  printf ("enter location B's longitude");
+  scanf("%lf", &lon2);
+
+/*
+daclaring the value as raidan to plug into the equation
+to convert degrees to raidans we will use r as the conversoun factor
+that being (pi/180) then multipying that by the degrees to get my raidens
+*/
+  double r=(M_PI/180);
+  double latr1=lat1*r;
+  double lonr1=lon1*r;
+  double latr2=lat2*r;
+  double lonr2=lon2*r;
+
+  printf("%lf", latr1);
+
+  double d = acos()
+
+  printf("%lf", lat1);
 
   return 0;
 }
